7a635e1e0772e9ae2cde91714d6902aabacf92c2
Maurits van Rees <maurits@vanrees.org>
2012-04-13 21:44:32 +0200

    Make this a package rediscounter.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+version = '0.1.dev0'
+
+setup(name='rediscounter',
+      version=version,
+      description="Counters in Redis",
+      long_description="""\
+""",
+      # Get strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[],
+      keywords='',
+      author='Maurits van Rees',
+      author_email='m.van.rees@zestsoftware.nl',
+      url='',
+      license='GPL',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+            'redis',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
