f2b221b03c4d5a9119f70c9600abeea4f3505688
Maurits van Rees <maurits@vanrees.org>
2012-04-13 23:20:54 +0200

    Count total visits and visits to the current page.

--- src/rediscounter/filter.py
+++ src/rediscounter/filter.py
@@ -6,6 +6,8 @@ _counter = Counter()
 
 
 class CounterFilter(Filter):
+
     def filter(self, environ, headers, data):
-        print _counter.visit('total')
+        _counter.visit('total')
+        _counter.visit(environ.get('PATH_INFO'))
         return data
