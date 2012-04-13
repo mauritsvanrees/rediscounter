1f14c9f45ce878eb7401cb66be20d8dc52427f62
Maurits van Rees <maurits@vanrees.org>
2012-04-14 01:26:08 +0200

    Show the text in the app prettier.

--- src/rediscounter/app.py
+++ src/rediscounter/app.py
@@ -9,6 +9,12 @@ from rediscounter.counter import Counter
 
 _counter = Counter()
 
+TEXT = """
+<p>User %(user)s visits page %(uid)r</p>
+<p>Total: %(total)d</p>
+<p>This page: %(page_total)d</p>
+<p>Visitors: %(visitors)s</p>"""
+
 
 def app(environ, start_response):
     qs = parse_qs(environ.get('QUERY_STRING', ''))
@@ -24,10 +30,15 @@ def app(environ, start_response):
         # Fall back to the path info for the unique id.
         uid = environ.get('PATH_INFO')
     total = _counter.visit('total')
-    page = _counter.visit(uid, user)
-    visitors = _counter.visitors(environ.get('PATH_INFO'))
-    text = "<p>Total: %d</p><p>This page: %d</p><p>Visitors: %s</p>" % (
-        total, page, ', '.join([v for v in visitors]))
+    page_total = _counter.visit(uid, user)
+    visitors = _counter.visitors(uid)
+    text = TEXT % dict(
+        user=user is None and 'Anonymous' or user,
+        uid=uid,
+        total=total,
+        page_total=page_total,
+        visitors=', '.join([v for v in visitors]),
+        )
     start_response('200 OK', [('content-type', 'text/html')])
     return [text]
 
