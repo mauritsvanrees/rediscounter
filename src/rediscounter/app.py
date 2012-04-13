6fd8688037d2eb2a4242d357a5590e03dc1af555
Maurits van Rees <maurits@vanrees.org>
2012-04-14 01:18:41 +0200

    Get a uid from the query parameters. Fall back to the path info.

--- src/rediscounter/app.py
+++ src/rediscounter/app.py
@@ -12,11 +12,19 @@ _counter = Counter()
 
 def app(environ, start_response):
     qs = parse_qs(environ.get('QUERY_STRING', ''))
+    # Get a user from the query parameters.
     user = qs.get('user')
     if isinstance(user, list):
         user = user[0]
+    # Get a unique id for which we should update the counter.
+    uid = qs.get('uid')
+    if isinstance(uid, list):
+        uid = uid[0]
+    if not uid:
+        # Fall back to the path info for the unique id.
+        uid = environ.get('PATH_INFO')
     total = _counter.visit('total')
-    page = _counter.visit(environ.get('PATH_INFO'), user)
+    page = _counter.visit(uid, user)
     visitors = _counter.visitors(environ.get('PATH_INFO'))
     text = "<p>Total: %d</p><p>This page: %d</p><p>Visitors: %s</p>" % (
         total, page, ', '.join([v for v in visitors]))
