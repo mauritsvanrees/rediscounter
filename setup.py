38e047bb37ac80ee3c0c649f2d691e95c78d2883
Maurits van Rees <maurits@vanrees.org>
2012-04-13 22:48:31 +0200

    reindent setup.py

--- setup.py
+++ setup.py
@@ -2,29 +2,30 @@ from setuptools import setup, find_packages
 
 version = '0.1.dev0'
 
-setup(name='rediscounter',
-      version=version,
-      description="Counters in Redis",
-      long_description="""\
+setup(
+    name='rediscounter',
+    version=version,
+    description="Counters in Redis",
+    long_description="""\
 """,
-      # Get strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[],
-      keywords='',
-      author='Maurits van Rees',
-      author_email='m.van.rees@zestsoftware.nl',
-      url='',
-      license='BSD',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-            'PasteDeploy',
-            'WSGIFilter',
-            'redis',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    # Get strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[],
+    keywords='',
+    author='Maurits van Rees',
+    author_email='m.van.rees@zestsoftware.nl',
+    url='',
+    license='BSD',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+          'PasteDeploy',
+          'WSGIFilter',
+          'redis',
+          ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+    )
