0cf657749bf8e5075f5afba41d96fbc0a0b796af
Maurits van Rees <maurits@vanrees.org>
2012-04-13 23:38:35 +0200

    TODO

--- src/rediscounter/filter.py
+++ src/rediscounter/filter.py
@@ -8,6 +8,8 @@ _counter = Counter()
 class CounterFilter(Filter):
 
     def filter(self, environ, headers, data):
+        # TODO: use pipes to send all requests in one go.
+        # TODO: handle connection errors.
         total = _counter.visit('total')
         page = _counter.visit(environ.get('PATH_INFO'))
         # Insert data at a nice place.
